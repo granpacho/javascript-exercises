bcc2d7ae72a83eca96924c9b871302730dba8d3a
granpacho <omarasharif@gmail.com>
2026-06-02 00:41:02 -0400

    Create functions to convert temperture
    Created functions to convert tempertures from celsius
    to fahrenheit and from fahrenheit to celsius in order
    to follow the assignment guidelines.
    
    Created a helper function to round numbers to the
    first decimal place in order to follow assignment
    guidelines.

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,29 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  let tempCelsius = (tempFahrenheit - 32) * (5/9);
+
+  let tempRounded = roundNumber(tempCelsius);
+
+  return tempRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  let tempFahrenheit = (tempCelsius * (9/5) + 32);
+
+  let tempRounded = roundNumber(tempFahrenheit);
+
+  return tempRounded;
 };
+
+
+  /* 
+  Mulitplies number by 10 to get the first decimal to become a whole number. 
+  Then round to the nearest whole number. 
+  Lastly divides by 10 put last number as decimal.
+  */
+
+function roundNumber(number) {
+  return (Math.round(number * 10) / 10);
+}
 
 // Do not edit below this line
 module.exports = {
