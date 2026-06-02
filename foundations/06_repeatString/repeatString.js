92fbfc60d23058e17e776a2e73115c30cdd6088a
granpacho <omarasharif@gmail.com>
2026-06-01 21:39:45 -0400

    Add code to concatenate a string with itself
    Added code to concatenate a string with it self a
    certain amount of times determined by arguments in
    repeatString.js to follow assignment guidelines.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    let finalString = "";
 
+    if (num < 0) return "ERROR";
+
+    for (let i = 1; i <= num; i++) finalString += word;
+
+    return finalString;
 };
 
 // Do not edit below this line
