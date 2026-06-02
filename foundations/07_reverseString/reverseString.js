9f0ef783b805d2231d837ff01fa730fcb32e3cf3
granpacho <omarasharif@gmail.com>
2026-06-01 22:11:44 -0400

    Add code to reverse a string
    Created code to reverse a string given as an argument
    in order to follow assignment details for reverseString.js.

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split('');
+    let stringArrayLength = stringArray.length;
 
+    let finalString = "";
+
+    for (let i = 0; i < stringArrayLength; i++) {
+        finalString += stringArray.pop();
+    }
+
+    return finalString;
 };
 
 // Do not edit below this line
