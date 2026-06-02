531f3b44084080981bce1ea0cd324f35612d8030
granpacho <omarasharif@gmail.com>
2026-06-02 00:14:51 -0400

    Create function to sum all numbers between two numbers
    Created a function that takes in two numbers, and
    sums all the numbers in between the two numbers up
    to and including the numbers in order to meet assignment
    guidelines.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
 
+    if (!Number.isInteger(firstNumber) || !Number.isInteger(secondNumber) || firstNumber < 0 || secondNumber < 0) return "ERROR"
+
+    let smallerNumber = (firstNumber >= secondNumber) ? secondNumber : firstNumber;
+    let largerNumber = (firstNumber >= secondNumber) ? firstNumber : secondNumber;
+
+    for (; smallerNumber <= largerNumber; smallerNumber++) {
+        sum += smallerNumber;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
